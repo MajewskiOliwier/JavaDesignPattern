f711e97219b13aa007484749a9ee09edfee5c45a
BroZor <kubulba1988@gmail.com>
2026-03-06 09:19:06 +0100

    Initial files

---
+++ src/main/java/allnew/okk/OkkApplication.java
@@ -0,0 +1,13 @@
+package allnew.okk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/allnew/okk/OkkApplicationTests.java
@@ -0,0 +1,13 @@
+package allnew.okk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
