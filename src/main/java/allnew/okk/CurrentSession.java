841aa7d0a8c7a79bb9071e5259d677111a741167
Oliwier Majewski <126395061+MajewskiOliwier@users.noreply.github.com>
2026-03-08 08:47:17 +0000

    Added Singleton and test for it

---
+++ src/main/java/allnew/okk/CurrentSession.java
@@ -0,0 +1,46 @@
+package allnew.okk;
+
+public class CurrentSession {
+    private static CurrentSession instance;
+    private BaseAccount loggedAccount;
+
+    private CurrentSession(){}
+
+    public static CurrentSession getInstance(){
+        if(instance == null){
+            instance = new CurrentSession();
+        }
+
+        return instance;
+    }
+
+    public void register(BaseAccount account){
+        this.loggedAccount = account;
+    }
+
+    public void login(BaseAccount account){
+        this.loggedAccount = account;
+    }
+
+    public void logout(){
+        this.loggedAccount = null;
+    }
+
+    public PrivateAccount getAsPrivate() {
+        if (loggedAccount instanceof PrivateAccount p) return p;
+        throw new IllegalStateException("Logged account is not a PrivateAccount");
+    }
+
+    public CompanyAccount getAsCompany() {
+        if (loggedAccount instanceof CompanyAccount c) return c;
+        throw new IllegalStateException("Logged account is not a CompanyAccount");
+    }
+
+    public boolean isPrivateAccount() {
+        return loggedAccount instanceof PrivateAccount;
+    }
+
+    public boolean isCompanyAccount() {
+        return loggedAccount instanceof CompanyAccount;
+    }
+}
--- src/test/java/allnew/okk/Week1OliwierTests.java
+++ src/test/java/allnew/okk/Week1OliwierTests.java
@@ -2,8 +2,7 @@ package allnew.okk;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Week1OliwierTests {
     @Test
@@ -37,5 +36,54 @@ class Week1OliwierTests {
         assertEquals("pass123!", account.password);
         assertEquals("COMPANY1WEEK", account.getLegalName());
         assertEquals("1234567890", account.getVatNumber());
+    }
+
+    @Test
+    void TestSingletonForPrivateAccount(){
+        PrivateAccount account = AccountFactory.createPrivateAccount()
+                .Email("week1@gmail.com")
+                .Password("pass123!")
+                .Name("Oliwier")
+                .Surname("Majewski")
+                .build();
+
+        CurrentSession.getInstance().login(account);
+
+        assertNotNull(account);
+        assertTrue(CurrentSession.getInstance().isPrivateAccount());
+        if (CurrentSession.getInstance().isPrivateAccount()) {
+
+            PrivateAccount p = CurrentSession.getInstance().getAsPrivate();
+
+            assertEquals("week1@gmail.com", p.email);
+            assertEquals("pass123!", p.password);
+            assertEquals("Oliwier", p.getName());
+            assertEquals("Majewski", p.getSurname());
+            assertEquals(0, p.getSavedMoney());
+        }
+    }
+    
+    @Test
+    void TestSingletonForCompanyAccount(){
+        CompanyAccount account = AccountFactory.createCompanyAccount()
+                .Email("week1@gmail.com")
+                .Password("pass123!")
+                .setLegalName("COMPANY1WEEK")
+                .setVatNumber("1234567890")
+                .build();
+
+        CurrentSession.getInstance().login(account);
+
+        assertNotNull(account);
+        assertTrue(CurrentSession.getInstance().isCompanyAccount());
+        if (CurrentSession.getInstance().isCompanyAccount()) {
+
+            CompanyAccount c = CurrentSession.getInstance().getAsCompany();
+
+            assertEquals("week1@gmail.com", c.email);
+            assertEquals("pass123!", c.password);
+            assertEquals("COMPANY1WEEK", c.getLegalName());
+            assertEquals("1234567890", c.getVatNumber());
+        }
     }
 }
